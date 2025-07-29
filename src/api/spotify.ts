107eab5a2746b33d9c8d2c2fd48504571f48aa02
Peter Kostin <46546031+peterkost@users.noreply.github.com>
2025-07-29 15:54:10 -0600

    fix: use correct env var for token refresh

--- src/api/spotify.ts
+++ src/api/spotify.ts
@@ -58,7 +58,7 @@ const getToken = async (
   body.append("refresh_token", currentRefreshToken);
 
   const basicAuth = btoa(
-    `${env.SPOTIFY_CLIENT_SECRET}:${env.SPOTIFY_CLIENT_SECRET}`,
+    `${env.SPOTIFY_CLIENT_ID}:${env.SPOTIFY_CLIENT_SECRET}`,
   );
 
   const response = await fetch("https://accounts.spotify.com/api/token", {
