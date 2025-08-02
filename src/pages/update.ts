72904d8d05b4481abfe7ac6a1aeffca37b291d36
Peter Kostin <46546031+peterkost@users.noreply.github.com>
2025-08-02 08:58:59 -0600

    chore: delete update endpoint

--- src/pages/update.ts
+++
@@ -1,8 +0,0 @@
-export const prerender = false;
-import { updateWeeklyPlayed } from "../api/spotify.ts";
-
-export async function GET(context) {
-  const runtime = context.locals.runtime;
-  await updateWeeklyPlayed(runtime.env);
-  return new Response("Some body");
-}
